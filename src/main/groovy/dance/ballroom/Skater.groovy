0adf4516bdf66833cd899e8fe7d53d2ccf14e5a3
ren-motomura <ren.motomura@gmail.com>
2016-05-22 17:08:04 +0900

    Mod

--- src/main/groovy/dance/ballroom/Skater.groovy
+++ src/main/groovy/dance/ballroom/Skater.groovy
@@ -1,15 +1,31 @@
-package com.example
+package dance.ballroom
 
 import groovy.swing.SwingBuilder
 import javax.swing.*
+import java.awt.BorderLayout
 
 class Skater {
     static void main(String[] args) {
+      def skater = new Skater()
+    }
+
+    private JFrame mainFrame
+
+    Skater() {
       def swingBuilder = new SwingBuilder()
-      swingBuilder.frame(title: 'Skater',
+      mainFrame = swingBuilder.frame(title: 'Skater',
                          defaultCloseOperation: JFrame.EXIT_ON_CLOSE,
                          size: [400, 500],
+                         locationRelativeTo: null,
                          show: true) {
+        borderLayout()
+
+        panel(constraints: BorderLayout.NORTH) {
+          button('OK', icon: imageIcon(url: getClass().getResource('../../red.jpg')))
+        }
+        panel(constraints: BorderLayout.SOUTH) {
+          button('NG')
+        }
       }
     }
 }
